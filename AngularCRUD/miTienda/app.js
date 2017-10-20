af8e132e9208dcc8307afdfce2757d59ee5be4ae
Juan-Guerra <studying.john@gmail.com>
2017-10-20 15:30:31 -0300

    0.0.2

--- AngularCRUD/miTienda/app.js
+++ AngularCRUD/miTienda/app.js
@@ -1,1 +1,66 @@
-var app = angular.module ("miTienda", ["ngRoute"],["angular-md5"],["ngCookies"]);
+var app = angular.module(
+  "miTienda",
+  ["ngRoute"],
+  ["angular-md5"],
+  ["ngCookies"]
+);
+
+app.config([
+  "$routeProvider",
+  "$locationProvider",
+  function($routeProvider, $locationProvider) {
+    $routeProvider
+      .when("/", {
+        templateUrl: "/index.html",
+        controller: "centralController"
+      })
+      .when("/registro", {
+        templateUrl: "/usuarios/registro/registro.html",
+        controller: "/usuarios/registro/registroController"
+      })
+      .when("/login", {
+        templateUrl: "/central/login.html",
+        controller: "/usuarios/loginController"
+      })
+      .when("/locales", {
+        templateUrl: "/locales/locales.html",
+        controller: "/locales/localesController"
+      })
+      .when("/discos", {
+        templateUrl: "/discos/discos.html",
+        controller: "discosController"
+      })
+      .when("/discos/:id", {
+        templateUrl: "/discos/discosDetalle.html",
+        controller: "/discos/discosDetalleController"
+      })
+      .when("/usuarios", {
+        templateUrl: "/usuarios/usuarios.html",
+        controller: "usuariosController"
+      })
+      .otherwise({ redirectTo: "/" });
+  }
+]);
+
+app.run([
+  "$rootScope",
+  "$location",
+  "$cookies",
+  "$http",
+  function($rootScope, $location, $cookies, $http) {
+    $rootScope.globals = $cookies.getObject("globals") || {};
+    if ($rootScope.globals.currentUser) {
+      $http.defaults.headers.common['Authorization'] =
+        "Basic" + $rootScope.globals.currentuser.authdata;
+    }
+
+    $rootScope.$on("$locationChangeStart", function(event, next, current) {
+      var restrictedPage =
+        $.inArray($location.path(), ["/login", "/registro"]) === -1;
+      var loggedIn = $rootScope.globals.currentUser;
+      if (restrictedPage && !loggedIn) {
+        $location.path("/login");
+      }
+    });
+  }
+]);
--- AngularCRUD/miTienda/locales/localesController.js
+++ AngularCRUD/miTienda/locales/localesController.js
@@ -1,10 +1,43 @@
-app.controller("localesController", ["$scope","localesController", function($scope,localesController) {
-    $scope.locales.direccion = direccion;
-    var localeS= $scope.locales.direccion;
+app.controller("localesController", [
+  "$scope",
+  "localesServicio",
+  function($scope, localesServicio) {
+      
+    // LOCALES//
 
-    function agregarLocal(localeS){
-        $scope.locales
-    }
-   
-    
-});
+    $scope.localesS = {};
+    $scope.localesModelo = {};
+    $scope.localesSOptions = localesServicio.dameOpcionesDelocalesServicio();
+
+    $scope.selected = {};
+
+    localesServicio.dbGetLocales().then(function(response) {
+      $scope.localesS = response.data;
+    });
+
+    $scope.agregarLocal = function() {
+      var modal = $scope.localesModelo;
+
+      localesServicio.dbPostLocal(modal).then(function(response) {
+        $scope.localesS.push(response.data);
+      });
+    };
+
+    $scope.oneSelected = function(modalSelected) {
+      modalSelected.selected = true;
+    };
+
+    $scope.borrarLocal = function(index, modalSelected) {
+      localesServicio.dbDeleteLocal(modalSelected).then(function(response) {
+        $scope.localesS.splice(index, 1);
+        modalSelected.selected = false;
+      });
+    };
+
+    $scope.editarLocal = function(modalSelected) {
+      localesServicio.dbPutLocal(modalSelected).then(function(response) {
+        modalSelected.selected = false;
+      });
+    };
+  }
+]);
